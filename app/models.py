07831ffeee57a197f9c116fe4fc3032fc7179987
lamar133 <lamar_johnson133@yahoo.ca>
2018-02-07 23:51:52 -0500

    added contestants table to models

--- app/models.py
+++ app/models.py
@@ -1,11 +1,39 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, ForeignKey, Integer, String, Float, Boolean
-from sqlalchemy import Index
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Index, create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker
+import logging
+
+##################################
+########## ENGINE SETUP ##########
+##################################
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'my_database',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+engine_config = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+engine = create_engine(engine_config, echo = True)
+
+###################################
+########## SESSION SETUP ##########
+###################################
+
+Session = sessionmaker(bind = engine)
+session = Session()
+
+############################################
+########## DEFINE DATABASE MODELS ##########
+############################################
 
 Base = declarative_base()
 
-class RealityShows(Base):
+class RealityShow(Base):
     __tablename__ = 'reality_shows'
     show = Column(String(250), index = True, primary_key = True)
     contestant = Column(String(250), index = True, primary_key = True)
@@ -13,14 +41,20 @@ class RealityShows(Base):
     num_pos_tweets = Column(Integer)
     num_neg_tweets = Column(Integer)
     
-
-class Tweets(Base):
+class Contestant(Base):
+    full_name = Column(String(250))
+    alternative_names = Column(String(250), index = True, primary_key = True)
+    
+class Tweet(Base):
     __tablename__ = 'tweets'
     tweet_id = Column(Integer, index = True, primary_key = True)
     tweet = Column(String(250))
     sentiment = Column(String(250))
     date_time = Column(String(250))
 
+###############################################
+########## CREATE TABLES IN DATABASE ##########
+###############################################
 
 #def addAwardShowData():
 #    golden_globes_award_1 = Awards(
@@ -28,4 +62,7 @@ class Tweets(Base):
 #        nominees = ["Call Me By Your Name", "Dunkirk", "The Post", "The Shape of Water", "Three Billboards Outside Ebbing, Missouri"]
 #    )
 
- 
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+    
+    reality_show = RealityShow()
